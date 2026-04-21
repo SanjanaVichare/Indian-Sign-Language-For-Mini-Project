3b30fa310f44edd73b32bd76342aefb0c8d51554
Sanjanaaa <sanjanastudys@gmail.com>
2026-04-22 00:57:12 +0530

    pin mediapipe version

--- streamlit_app.py
+++ streamlit_app.py
@@ -31,9 +31,9 @@ def load_model():
 model = load_model()
 
 # ── MediaPipe
-mp_hands = mp.solutions.hands
-mp_drawing = mp.solutions.drawing_utils
-hands = mp_hands.Hands(static_image_mode=True, min_detection_confidence=0.5)
+from mediapipe.python.solutions import hands as mp_hands_module
+from mediapipe.python.solutions import drawing_utils as mp_drawing
+hands = mp_hands_module.Hands(static_image_mode=True, min_detection_confidence=0.5)
 
 labels_dict = {i: chr(65+i) for i in range(26)}  # A-Z
 
